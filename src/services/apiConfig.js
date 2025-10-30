3cc923cec7ed023f89aca61c368955b7d1105ac3
arunvenkatesh01071999 <arunvenkatesh01071999@gmail.com>
2025-10-30 17:19:16 +0530

    feed back frontend

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL ='https://feedback-backend-1-tz7h.onrender.com/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 
 const api = axios.create({
